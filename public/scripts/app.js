4eb551edb66590c74bd3ea56bfade21b0dbcdeb0
Angela Larryant <angelarryant@gmail.com>
2018-10-12 18:02:48 +0000

    changed colour scheme and theme to borker

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
 
   function createTweetElement({ user: { avatars, name, handle }, content, created_at }) {
-    const newDate = new Date(created_at); // converts Unix time to legible date
+    const time = moment(created_at).fromNow(); // converts Unix time to legible date
     const $article = $('<article>').addClass("tweet");
     const $header = $('<header>');
     const $image = $('<img>').attr('src', avatars.small);
     const $headerOne = $('<h1>').text(name);
     const $headerFive = $('<h5>').text(handle);
     const $paragraph = $('<p>').text(content.text);
-    const $footer = $('<footer>').text(`Posted on ${newDate}.`);
+    const $footer = $('<footer>').addClass("dateStamp").text(`Posted ${time}.`);
+    const $flagIcon = $('<div class="social" id="flag"><i class="fab fa-font-awesome-flag"></i></div>');
+    const $shareIcon = $('<div class="social" id="share"><i class="fas fa-retweet"></i></div>');
+    const $heartIcon = $('<div class="social" id="heart"><i class="fas fa-heart"></i></div>');
 
     // construct header components first so that everything nests correctly
     let $headerComponents = $header.append($image).append($headerOne).append($headerFive);
+    let $footerComponents = $footer.append($flagIcon).append($shareIcon).append($heartIcon);
     // final assembly
-    return $article.append($headerComponents).append($paragraph).append($footer);
+    return $article.append($headerComponents).append($paragraph).append($footerComponents);
   }
 
   function renderTweets(tweets) {
@@ -38,13 +42,13 @@ $(document).ready(function () {
     event.preventDefault();
 
     let data = $(event.target).serialize();
-    let input = $("textarea[name=text]").val();
+    let input = $("textarea[name=text]").val(); // text entered in text area
 
     if (input.length === 0) {
-      $(".error").text("ERROR! Please enter a message before tweeting.").slideDown("slow");
+      $(".error").text("BORK! Is it a bork if no one can hear you?").slideDown("slow");
     }
     else if (input.length > 140) {
-      $(".error").text("ERROR! Too many characters.").slideDown("slow");
+      $(".error").text("BORK! Too long, noise complaint incoming!").slideDown("slow");
     } else {
       $(".error").slideUp("slow");
       $.ajax('/tweets', {method: 'POST', data: data, success:
@@ -55,6 +59,7 @@ $(document).ready(function () {
     }
   });
 
+// compose button
   $("button").click(function(){
     $(".new-tweet").slideToggle("slow");
     $("textarea").focus();
